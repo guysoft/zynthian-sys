11ffd23a5f9e60c0f889550f977039537d8cfa14
Guy Sheffer <guysoft@gmail.com>
2018-01-31 21:54:14 +0200

    Attempt to hard-fix jpmidi and lv2-only/no-ladspa errors

--- scripts/recipes/install_guitarix.sh
+++ scripts/recipes/install_guitarix.sh
@@ -2,7 +2,7 @@
 cd $ZYNTHIAN_PLUGINS_SRC_DIR
 git clone http://git.code.sf.net/p/guitarix/git guitarix-git
 cd guitarix-git/trunk
-./waf configure --no-lv2-gui --lv2-only --disable-sse --lv2dir=$ZYNTHIAN_PLUGINS_DIR/lv2 --no-avahi --no-bluez --no-ladspa --no-faust 
+./waf configure --no-lv2-gui --disable-sse --lv2dir=$ZYNTHIAN_PLUGINS_DIR/lv2 --no-avahi --no-bluez --no-faust 
 ./waf build
 sudo ./waf install
 ./waf clean
--- scripts/recipes/install_jpmidi.sh
+++ scripts/recipes/install_jpmidi.sh
@@ -1,6 +1,8 @@
 cd $ZYNTHIAN_SW_DIR
 if [ ! -d "jpmidi" ]; then
-	wget http://juliencoder.de/jpmidi/jpmidi-0.2.tar.gz
+	# wget http://juliencoder.de/jpmidi/jpmidi-0.2.tar.gz
+	wget https://github.com/jerash/jpmidi/archive/master.tar.gz
+	mv master.tar.gz jpmidi-0.2.tar.gz
 	tar xfvz jpmidi-0.2.tar.gz
 	cd jpmidi
 	./configure
