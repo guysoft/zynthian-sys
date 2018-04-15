ce451b91b15b8f20ff70890036c81ab9062a2077
Guy Sheffer <guysoft@gmail.com>
2018-04-15 18:32:54 +0300

    e57a43cc9b0dc250e3158e897e1457052946efa1 Add new recipes for DISTRHO and DFP plugins.

--- scripts/recipes/install_sosynth.sh
+++ scripts/recipes/install_sosynth.sh
@@ -4,5 +4,5 @@ git clone https://github.com/jeremysalwen/So-synth-LV2
 cd So-synth-LV2
 sed -i -- 's/^INSTALLDIR = \$(DESTDIR)\/usr\/lib\/lv2\//INSTALLDIR = \/zynthian\/zynthian-plugins\/lv2\//' Makefile
 make -j 4
-make install
+sudo make install
 make clean
